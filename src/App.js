a8ef389be2eef73c93bd11acf2a16c2685332b59
Marcy Sutton <holla@marcysutton.com>
2023-06-14 14:42:37 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import IndexPage from './pages/index'
+
+export function App() {
+    return (
+        <>
+            <header>
+                <h1>hello world!</h1>
+            </header>
+            <main>
+                <IndexPage />
+            </main>
+        </>
+    )
+}
