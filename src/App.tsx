5be39d54abbc84c096f86697b399f63d87cbe0df
Marcy Sutton <holla@marcysutton.com>
2024-07-07 13:59:53 -0700

    exercise files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import TransactionListContainer from "./pages/transactions/transaction-list-container";
+
+export function App() {
+    return (
+        <>
+            <header>
+                <h1>Example App</h1>
+            </header>
+            <main>
+                <TransactionListContainer />                
+            </main>
+        </>
+    )
+}
--- src/pages/example/index.tsx
+++ src/pages/example/index.tsx
@@ -1,4 +1,6 @@
-import React, {useEffect, useState} from 'react'
+import {useEffect, useState} from 'react'
+
+import './example.css';
 
 const IndexPage = () => {
     const [inputValues, setInputValues] = useState({
